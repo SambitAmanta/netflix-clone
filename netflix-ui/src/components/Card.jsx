d2ab9b69e9fc5994af4ac36bbad8518ab6935fbb
SambitAmanta <69429176+SambitAmanta@users.noreply.github.com>
2023-08-27 11:50:42 +0530

    Update Card.jsx

--- netflix-ui/src/components/Card.jsx
+++ netflix-ui/src/components/Card.jsx
@@ -26,7 +26,7 @@ export default React.memo(function Card({ index, movieData, isLiked = false }) {
 
   const addToList = async () => {
     try {
-      await axios.post("http://localhost:5000/api/user/add", {
+      await axios.post("https://netflix-backend-kv5o.onrender.com/api/user/add", {
         email,
         data: movieData,
       });
